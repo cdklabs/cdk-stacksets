f211ecaadeab4c158f5bd1d49fdadc2c3e22ccf7
Kaizen Conroy <zen.conroy@gmail.com>
2023-01-04 16:36:55 -0500

    chore: minor projenrc updates

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
+
+const project = new CdklabsConstructLibrary({
+  projenrcTs: true,
+  author: 'AWS',
+  authorAddress: 'aws-cdk-dev@amazon.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-stacksets',
+  repositoryUrl: 'https://github.com/cdklabs/cdk-stacksets.git',
+
+  deps: ['cdklabs-projen-project-types'],
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
