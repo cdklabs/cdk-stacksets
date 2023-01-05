3264c3c62ec4c78c8b949faf89a7088c3483b16a
corymhall <43035978+corymhall@users.noreply.github.com>
2023-01-05 16:46:36 +0000

    feat: initial implementation of a StackSet L2 construct
    See README.md

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,7 +4,7 @@ const project = new CdklabsConstructLibrary({
   projenrcTs: true,
   author: 'AWS',
   authorAddress: 'aws-cdk-dev@amazon.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.45.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-stacksets',
   repositoryUrl: 'https://github.com/cdklabs/cdk-stacksets.git',
---
+++ test/stack-set.test.ts
@@ -0,0 +1,273 @@
+import {
+  App, Stack,
+} from 'aws-cdk-lib';
+
+import { Template } from 'aws-cdk-lib/assertions';
+import { DeploymentType, StackSet, StackSetTarget, StackSetTemplate } from '../src/stackset';
+import { StackSetStack } from '../src/stackset-stack';
+
+test('default', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  new StackSet(stack, 'StackSet', {
+    target: StackSetTarget.fromAccounts({
+      regions: ['us-east-1'],
+      accounts: ['11111111111'],
+      parameterOverrides: {
+        Param1: 'Value1',
+      },
+    }),
+    template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+  });
+
+  Template.fromStack(stack).hasResourceProperties('AWS::CloudFormation::StackSet', {
+    ManagedExecution: { Active: true },
+    PermissionModel: 'SELF_MANAGED',
+    TemplateURL: {
+      'Fn::Sub': 'https://s3.${AWS::Region}.${AWS::URLSuffix}/cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}/44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a.json',
+    },
+    StackInstancesGroup: [{
+      Regions: ['us-east-1'],
+      DeploymentTargets: {
+        Accounts: ['11111111111'],
+        AccountFilterType: 'INTERSECTION',
+      },
+    }],
+  });
+});
+
+test('self managed stackset creates adminRole by default', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  new StackSet(stack, 'StackSet', {
+    target: StackSetTarget.fromAccounts({
+      regions: ['us-east-1'],
+      accounts: ['11111111111'],
+      parameterOverrides: {
+        Param1: 'Value1',
+      },
+    }),
+    template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+  });
+
+  Template.fromStack(stack).hasResourceProperties('AWS::CloudFormation::StackSet', {
+    ManagedExecution: { Active: true },
+    PermissionModel: 'SELF_MANAGED',
+    AdministrationRoleARN: { 'Fn::GetAtt': ['AdminRole38563C57', 'Arn'] },
+    TemplateURL: {
+      'Fn::Sub': 'https://s3.${AWS::Region}.${AWS::URLSuffix}/cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}/44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a.json',
+    },
+    StackInstancesGroup: [{
+      Regions: ['us-east-1'],
+      DeploymentTargets: {
+        Accounts: ['11111111111'],
+        AccountFilterType: 'INTERSECTION',
+      },
+    }],
+  });
+  Template.fromStack(stack).hasResourceProperties('AWS::IAM::Role', {
+    AssumeRolePolicyDocument: {
+      Statement: [
+        {
+          Effect: 'Allow',
+          Principal: { Service: 'cloudformation.amazonaws.com' },
+          Action: 'sts:AssumeRole',
+        },
+      ],
+    },
+  });
+});
+
+test('self managed stackset with disabled regions', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  new StackSet(stack, 'StackSet', {
+    target: StackSetTarget.fromAccounts({
+      regions: ['us-east-1', 'af-south-1'],
+      accounts: ['11111111111'],
+      parameterOverrides: {
+        Param1: 'Value1',
+      },
+    }),
+    template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+  });
+
+  Template.fromStack(stack).hasResourceProperties('AWS::CloudFormation::StackSet', {
+    ManagedExecution: { Active: true },
+    PermissionModel: 'SELF_MANAGED',
+    AdministrationRoleARN: { 'Fn::GetAtt': ['AdminRole38563C57', 'Arn'] },
+    TemplateURL: {
+      'Fn::Sub': 'https://s3.${AWS::Region}.${AWS::URLSuffix}/cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}/44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a.json',
+    },
+    StackInstancesGroup: [{
+      Regions: ['us-east-1', 'af-south-1'],
+      DeploymentTargets: {
+        Accounts: ['11111111111'],
+        AccountFilterType: 'INTERSECTION',
+      },
+    }],
+  });
+  Template.fromStack(stack).hasResourceProperties('AWS::IAM::Role', {
+    AssumeRolePolicyDocument: {
+      Statement: [
+        {
+          Effect: 'Allow',
+          Principal: { Service: 'cloudformation.amazonaws.com' },
+          Action: 'sts:AssumeRole',
+        },
+        {
+          Effect: 'Allow',
+          Principal: {
+            Service: 'cloudformation.af-south-1.amazonaws.com',
+          },
+          Action: 'sts:AssumeRole',
+        },
+      ],
+    },
+  });
+});
+
+test('service managed stackset', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  new StackSet(stack, 'StackSet', {
+    target: StackSetTarget.fromAccounts({
+      regions: ['us-east-1'],
+      accounts: ['11111111111'],
+      parameterOverrides: {
+        Param1: 'Value1',
+      },
+    }),
+    deploymentType: DeploymentType.serviceManaged(),
+    template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+  });
+
+  Template.fromStack(stack).hasResourceProperties('AWS::CloudFormation::StackSet', {
+    ManagedExecution: { Active: true },
+    PermissionModel: 'SERVICE_MANAGED',
+    CallAs: 'DELEGATED_ADMIN',
+    AutoDeployment: {
+      Enabled: true,
+      RetainStacksOnAccountRemoval: true,
+    },
+    TemplateURL: {
+      'Fn::Sub': 'https://s3.${AWS::Region}.${AWS::URLSuffix}/cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}/44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a.json',
+    },
+    StackInstancesGroup: [{
+      ParameterOverrides: [{
+        ParameterKey: 'Param1',
+        ParameterValue: 'Value1',
+      }],
+      Regions: ['us-east-1'],
+      DeploymentTargets: {
+        Accounts: ['11111111111'],
+        AccountFilterType: 'INTERSECTION',
+      },
+    }],
+  });
+});
+
+test('service managed stackset with options', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  new StackSet(stack, 'StackSet', {
+    target: StackSetTarget.fromAccounts({
+      regions: ['us-east-1'],
+      accounts: ['11111111111'],
+      parameterOverrides: {
+        Param1: 'Value1',
+      },
+    }),
+    deploymentType: DeploymentType.serviceManaged({
+      delegatedAdmin: false,
+      autoDeployEnabled: false,
+    }),
+    template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+  });
+
+  Template.fromStack(stack).hasResourceProperties('AWS::CloudFormation::StackSet', {
+    ManagedExecution: { Active: true },
+    PermissionModel: 'SERVICE_MANAGED',
+    CallAs: 'SELF',
+    AutoDeployment: {
+      Enabled: false,
+    },
+    TemplateURL: {
+      'Fn::Sub': 'https://s3.${AWS::Region}.${AWS::URLSuffix}/cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}/44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a.json',
+    },
+    StackInstancesGroup: [{
+      Regions: ['us-east-1'],
+      DeploymentTargets: {
+        Accounts: ['11111111111'],
+        AccountFilterType: 'INTERSECTION',
+      },
+    }],
+  });
+});
+
+test('service managed stackset throws error if autoDeployRetainStacks is provided and autoDeploy is disabled', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  expect(() => {
+    new StackSet(stack, 'StackSet', {
+      target: StackSetTarget.fromAccounts({
+        regions: ['us-east-1'],
+        accounts: ['11111111111'],
+        parameterOverrides: {
+          Param1: 'Value1',
+        },
+      }),
+      deploymentType: DeploymentType.serviceManaged({
+        delegatedAdmin: false,
+        autoDeployEnabled: false,
+        autoDeployRetainStacks: true,
+      }),
+      template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+    });
+  }).toThrow(/autoDeployRetainStacks only applies if autoDeploy is enabled/);
+});
+
+test('fromOrganizations default', () => {
+  const app = new App();
+  const stack = new Stack(app);
+
+  new StackSet(stack, 'StackSet', {
+    target: StackSetTarget.fromOrganizationalUnits({
+      regions: ['us-east-1'],
+      organizationalUnits: ['ou-1111111'],
+      parameterOverrides: {
+        Param1: 'Value1',
+      },
+    }),
+    deploymentType: DeploymentType.serviceManaged({
+      delegatedAdmin: false,
+      autoDeployEnabled: false,
+    }),
+    template: StackSetTemplate.fromStackSetStack(new StackSetStack(stack, 'Stack')),
+  });
+
+  Template.fromStack(stack).hasResourceProperties('AWS::CloudFormation::StackSet', {
+    ManagedExecution: { Active: true },
+    PermissionModel: 'SERVICE_MANAGED',
+    CallAs: 'SELF',
+    AutoDeployment: {
+      Enabled: false,
+    },
+    TemplateURL: {
+      'Fn::Sub': 'https://s3.${AWS::Region}.${AWS::URLSuffix}/cdk-hnb659fds-assets-${AWS::AccountId}-${AWS::Region}/44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a.json',
+    },
+    StackInstancesGroup: [{
+      Regions: ['us-east-1'],
+      DeploymentTargets: {
+        AccountFilterType: 'NONE',
+      },
+    }],
+  });
+
+});
