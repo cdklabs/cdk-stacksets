d252fc0a3288ec851161e6335875cdc05cab1d3f
corymhall <43035978+corymhall@users.noreply.github.com>
2023-01-05 14:30:35 +0000

    don't publish yet

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,5 +10,6 @@ const project = new CdklabsConstructLibrary({
   repositoryUrl: 'https://github.com/cdklabs/cdk-stacksets.git',
   minNodeVersion: '14.18.0',
   devDeps: ['cdklabs-projen-project-types'],
+  publishDryRun: true,
 });
 project.synth();
