de8b7fb3c2b88d01f861cdf1cecd995f0977dc8d
Cory Hall <43035978+corymhall@users.noreply.github.com>
2023-01-17 08:58:24 -0500

    chore: make package public and enable release (#6)

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,7 +10,8 @@ const project = new CdklabsConstructLibrary({
   repositoryUrl: 'https://github.com/cdklabs/cdk-stacksets.git',
   minNodeVersion: '14.18.0',
   devDeps: ['cdklabs-projen-project-types', '@aws-cdk/integ-tests-alpha', '@aws-cdk/integ-runner@2.60.0', 'aws-cdk@2.60.0'],
-  publishDryRun: true,
+  publishDryRun: false,
+  private: false,
 });
 
 const integSnapshotTask = project.addTask('integ', {
