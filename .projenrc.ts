65291c267aabf2dfa66a8782da52dc649cb5657e
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2023-02-24 12:43:50 -0500

    chore: upgrade cdklabs-projen-project-types (#32)
    Upgrades `cdklabs-projen-project-types` from `0.0.9` to `0.1.21`. This has to be done manually because projen's upgrade-main workflow refuses to upgrade `cdklabs-projen-project-types` after it was pinned to `^0.0.9`.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
 import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
+import { JsonPatch } from 'projen';
 
 const project = new CdklabsConstructLibrary({
   projenrcTs: true,
@@ -9,31 +10,15 @@ const project = new CdklabsConstructLibrary({
   name: 'cdk-stacksets',
   repositoryUrl: 'https://github.com/cdklabs/cdk-stacksets.git',
   minNodeVersion: '14.18.0',
-  devDeps: ['cdklabs-projen-project-types@^0.0.9', '@aws-cdk/integ-tests-alpha', '@aws-cdk/integ-runner@2.60.0', 'aws-cdk@2.60.0'],
+  devDeps: ['cdklabs-projen-project-types', '@aws-cdk/integ-tests-alpha', '@aws-cdk/integ-runner@2.60.0', 'aws-cdk@2.60.0'],
   publishDryRun: false,
   private: false,
 });
 
-const integSnapshotTask = project.addTask('integ', {
-  description: 'Run integration snapshot tests',
-  exec: 'yarn integ-runner --language typescript',
-});
+const tasksJson = project.tryFindObjectFile('.projen/tasks.json');
+tasksJson?.patch(JsonPatch.add('/tasks/integ:update/requiredEnv', [
+  'INTEG_DEPLOYMENT_ACCOUNT',
+  'INTEG_TARGET_ACCOUNT',
+]));
 
-project.addTask('integ:update', {
-  description: 'Run and update integration snapshot tests',
-  exec: 'yarn integ-runner --language typescript --update-on-failed',
-  requiredEnv: [
-    'INTEG_DEPLOYMENT_ACCOUNT',
-    'INTEG_TARGET_ACCOUNT',
-  ],
-});
-
-const rosettaTask = project.addTask('rosetta:extract', {
-  description: 'Test rosetta extract',
-  exec: 'yarn --silent jsii-rosetta extract',
-});
-
-project.testTask.spawn(integSnapshotTask);
-project.postCompileTask.spawn(rosettaTask);
-project.addGitIgnore('.jsii.tabl.json');
 project.synth();
