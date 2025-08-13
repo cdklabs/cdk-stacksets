9a62d7064e028ac53f06089851cc4072e1264b60
Ray Kohler <rkohler@renovo1.com>
2025-08-13 16:10:34 -0400

    fix: `undefinedIfNoKeys` requires a definitely-defined object

--- src/stackset.ts
+++ src/stackset.ts
@@ -645,7 +645,7 @@ export class StackSet extends Resource implements IStackSet {
       managedExecution: {
         Active: props.managedExecution ?? true,
       },
-      operationPreferences: undefinedIfNoKeys(props.operationPreferences),
+      operationPreferences: props.operationPreferences ? undefinedIfNoKeys(props.operationPreferences) : undefined,
       stackSetName: this.physicalName,
       capabilities: props.capabilities,
       permissionModel: deploymentTypeConfig.permissionsModel,
